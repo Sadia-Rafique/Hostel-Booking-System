197be1d15c6618a88a2ec2c81caa421db8577bde
Sadia-Rafique <sadiarafque@gmail.com>
2026-06-23 10:22:14 +0500

    fix variables

--- cloudconflict.js
+++ cloudconflict.js
@@ -4,7 +4,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 // Configure Cloudinary credentials
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
+    api_key: process.env.Api_Key,
     api_secret: process.env.API_SECRET 
 });
 
